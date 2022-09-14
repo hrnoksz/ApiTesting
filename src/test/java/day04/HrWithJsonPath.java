bcd8bf2b6f6b5e9503d9f5c7b4dc6fc628d720d3
hrnoksz <harun1973@yahoo.com>
2022-09-14 23:01:37 +0300

    HrWithJsonPath.java test2 done

--- src/test/java/day04/HrWithJsonPath.java
+++ src/test/java/day04/HrWithJsonPath.java
@@ -57,6 +57,37 @@ public class HrWithJsonPath {
         List<Integer> list2 = jsonPath.getList("items.findAll{it.region_id == 3}.country_name");
         System.out.println("list2 = " + list2);
 
+    }
 
+    @Test
+    public void test2() {
+
+        Response response = given().accept(ContentType.JSON)
+                .when().get("/employees");
+        JsonPath jsonPath = response.jsonPath();
+        jsonPath.prettyPrint();
+
+        System.out.println("jsonPath.getString(\"items[2].first_name\") = "
+                + jsonPath.getString("items[2].first_name"));
+
+        System.out.println("jsonPath.getInt(\"limit\") = " + jsonPath.getInt("limit"));
+
+        List<String> list = jsonPath.getList("items.last_name");
+        for (String eachLastname : list) {
+            System.out.println("eachLastname = " + eachLastname);
+        }
+
+        List<String> list1 = jsonPath.getList("items.findAll{it.job_id == \"IT_PROG\"}.email");
+        System.out.println("list1 = " + list1);
+
+       List<String> list2 = jsonPath.getList("items.findAll{it.salary > 8000}.first_name");
+       System.out.println("list2 = " + list2);
+       System.out.println("list2.size() = " + list2.size());
+
+       String maxLastname = jsonPath.getString("items.max{it.salary}.last_name");
+       System.out.println("maxLastname = " + maxLastname);
+
+       List<Integer> list3 = jsonPath.getList("items.findAll{it.salary > 13000}.employee_id");
+        System.out.println("list3 = " + list3);
     }
 }
