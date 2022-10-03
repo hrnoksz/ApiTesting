abacff6dd8ff3c764f483f21989e77f44f1676b0
hrnoksz <harun1973@yahoo.com>
2022-10-03 14:14:18 +0300

    SpartanWithAuthTest.java admin test is done

--- src/test/java/day08/SpartanWithAuthTest.java
+++ src/test/java/day08/SpartanWithAuthTest.java
@@ -4,24 +4,46 @@ import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-import com.google.gson.Gson;
-import day06.Spartan;
 import io.restassured.http.ContentType;
-import io.restassured.path.json.JsonPath;
-import io.restassured.response.Response;
-import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import utilities.ConfigurationReader;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public class SpartanWithAuthTest {
 
     @BeforeAll
     public static void init(){
         baseURI = ConfigurationReader.getProperty("sptn_url");
+    }
+    @DisplayName("GET /api/spartans as a public user(guest)")
+    @Test
+    public void test1(){
+        try {
+            given()
+                    .accept(ContentType.JSON)
+            .when()
+                    .get("/api/spartans")
+            .then().statusCode(401)
+                    .log().all();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    @DisplayName("GET /api/spartans as admin user expect 200")
+    @Test
+    public void testAdmin(){
+        //how to pass admin admin as a username and password?
+        given()
+                .auth().basic("admin", "admin")
+                .and().accept(ContentType.JSON)
+        .when()
+                .get("/api/spartans")
+        .then()
+                .statusCode(200)
+                .log().all();
     }
 }
